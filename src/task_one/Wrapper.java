3a22425ef80df8e24aade4d3413544138736b814
Uladzimir Paliakou <to.polyakov.vova@gmail.com>
2017-06-14 11:07:58 +0300

    Create Wrapper.java

--- src/task_one/Wrapper.java
+++ src/task_one/Wrapper.java
@@ -5,11 +5,11 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Wrapper {
-
+// Почему ты впихнул main метод в класс обертки? Ты должен тут сделать класс который будет вовращать тебе потоки 
     public static void main(String[] args) throws IOException{
 
         ConstructHTML constructHTML = new ConstructHTML("kek");
-
+// Доставать переменные методом toString нельзя. Для этого используются get методы , которые тебе возвращают поле класса
         BufferedWriter writer = new BufferedWriter(new FileWriter(constructHTML.toString()));
 
         System.out.println(constructHTML.getName());
